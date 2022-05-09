12b5eef4d818c784450709cbeeff1c14a2073d0b
skshamagarwal <skshamagarwal@gmail.com>
2022-05-09 11:24:27 +0530

    Node Fs Package

---
+++ fsModule/fileRead.js
@@ -0,0 +1,10 @@
+var http = require('http')
+var fs = require('fs')
+
+http.createServer(function(req, res) {
+    fs.readFile('demo.html', function(error, data){
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.write(data);
+        return res.end();
+    })
+}).listen(4000);
